06f6a68c96a13df038cad4c34425865d23d15d38
NoahReyson <shadow.reyson@gmail.com>
2019-03-08 09:24:06 +0100

    GymnasiumColor list

--- Planificateur/src/main/java/fr/mrcraftcod/shcheduler/Parser.java
+++ Planificateur/src/main/java/fr/mrcraftcod/shcheduler/Parser.java
@@ -4,14 +4,16 @@ import fr.mrcraftcod.shcheduler.exceptions.IllegalCSVFormatException;
 import fr.mrcraftcod.shcheduler.exceptions.ParserException;
 import fr.mrcraftcod.shcheduler.model.*;
 import fr.mrcraftcod.shcheduler.utils.GymnasiumColor;
+import javafx.scene.paint.Color;
 
-import java.awt.*;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,7 +28,17 @@ import java.util.stream.Collectors;
 public class Parser{
 	private final String csvSeparator;
 	private final List<GymnasiumColor> colors = new ArrayList<>(){{
-		add(new GymnasiumColor());
+		add(new GymnasiumColor(Color.valueOf("#22c1c3"), Color.valueOf("#000000")));
+		add(new GymnasiumColor(Color.valueOf("#fdbb2d"), Color.valueOf("#000000")));
+		add(new GymnasiumColor(Color.valueOf("#833ab4"), Color.valueOf("#FFFFF")));
+		add(new GymnasiumColor(Color.valueOf("#F8CDDA"), Color.valueOf("#000000")));
+		add(new GymnasiumColor(Color.valueOf("#FF512F"), Color.valueOf("#000000")));
+		add(new GymnasiumColor(Color.valueOf("#F09819"), Color.valueOf("#000000")));
+		add(new GymnasiumColor(Color.valueOf("#1A2980"), Color.valueOf("#FFFFFF")));
+		add(new GymnasiumColor(Color.valueOf("#DD2476"), Color.valueOf("#000000")));
+		add(new GymnasiumColor(Color.valueOf("#403B4A"), Color.valueOf("#FFFFFF")));
+		add(new GymnasiumColor(Color.valueOf("#3CA55C"), Color.valueOf("#000000")));
+		add(new GymnasiumColor(Color.valueOf("#603813"), Color.valueOf("#FFFFFF")));
 	}};
 	
 	/**
